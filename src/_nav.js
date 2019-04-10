d7a3494058622a1a3fad6e5a641bac90ecff7aa7
Drivezy <drivezy@Drivezys-MacBook-Pro.local>
2019-04-11 03:53:14 +0530

    Added Gitignore

--- src/_nav.js
+++ src/_nav.js
@@ -11,13 +11,24 @@ export default {
     },
     {
       title: true,
-      name: 'Theme',
+      name: 'Core',
       wrapper: {            // optional wrapper object
         element: '',        // required valid HTML5 element tag
         attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
       },
       class: ''             // optional class names space delimited list for title item ex: "text-center"
     },
+    {
+      name: 'Menus',
+      url: '/theme/colors',
+      icon: 'icon-drop',
+    },
+    {
+      name: 'Column Types',
+      url: '/theme/colors',
+      icon: 'icon-drop',
+    },
+
     {
       name: 'Colors',
       url: '/theme/colors',
@@ -260,21 +271,6 @@ export default {
       url: '/dashboard',
       icon: 'icon-ban',
       attributes: { disabled: true },
-    },
-    {
-      name: 'Download CoreUI',
-      url: 'https://coreui.io/react/',
-      icon: 'icon-cloud-download',
-      class: 'mt-auto',
-      variant: 'success',
-      attributes: { target: '_blank', rel: "noopener" },
-    },
-    {
-      name: 'Try CoreUI PRO',
-      url: 'https://coreui.io/pro/react/',
-      icon: 'icon-layers',
-      variant: 'danger',
-      attributes: { target: '_blank', rel: "noopener" },
     },
   ],
 };
